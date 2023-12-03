e46d20aff903471b3a05d07b16f71fe4dfb3f96e
tanhua <tanhua_0201@youzan.com>
2023-12-03 18:31:07 +0800

    feat: docker -alpine

--- src/publish_docker.sh
+++ src/publish_docker.sh
@@ -1,3 +1,3 @@
-docker build  --pull -f "Dockerfile" -t docker-nginx-cerbot:latest "."
+docker build --pull -f "Dockerfile-alpine" -t docker-nginx-cerbot:latest "."
 docker tag docker-nginx-cerbot:latest registry.cn-hangzhou.aliyuncs.com/rancune/docker-nginx-cerbot:latest
 docker push registry.cn-hangzhou.aliyuncs.com/rancune/docker-nginx-cerbot:latest
